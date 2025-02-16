4512f3b4955b0ce7575cdbf052da4ddb59139645
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-02-16 18:39:40 +0100

    Base project

---
+++ ProprocessorDirectivesTests/ProprocessorDirectivesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProprocessorDirectivesTests.swift
+//  ProprocessorDirectivesTests
+//
+//  Created by Javier Calatrava on 16/2/25.
+//
+
+import Testing
+
+struct ProprocessorDirectivesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
